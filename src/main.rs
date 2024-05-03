240fc6e0e0216c7ac6a05c3c22772e8cd0a43ab6
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-05-02 21:20:21 -0400

    patch: test time_waited increments mutable

--- src/main.rs
+++ src/main.rs
@@ -19,20 +19,24 @@ const PATIENT_PROBABILITY: f32 = 0.2;
 
 fn main() {
     let mut rng = ChaCha8Rng::seed_from_u64(SEED);
-    // let vec_docs = DoctorFactory::generate_vec_doctors(10, &mut rng);
+    let vec_docs = DoctorFactory::generate_vec_doctors(10, &mut rng);
     let mut vec_patients: Vec<Patient> = Vec::new();
     let mut triage_queue = SchedulerSingleQueue::new();
 
+    dbg!(vec_docs);
+
     for tick in 0..MAX_TICKS {
-        // // Update time-waited for all patients and update position score
-        // vec_patients
-        //     .iter()
-        //     .map(|&mut pt| pt.increment_time_waited());
+        // Update time-waited for all patients and update position score
+        vec_patients
+            .iter_mut()
+            .for_each(|pt| pt.increment_time_waited());
 
         if rng.gen_range(0.0..1.0) < PATIENT_PROBABILITY {
             let patient = PatientFactory::create_patient(&mut rng, tick);
             triage_queue.push(&patient);
             vec_patients.push(patient);
         }
+
+        dbg!(&vec_patients);
     }
 }
